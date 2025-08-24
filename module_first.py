c3a50a4140076201d65c1f721baf256320ad6698
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-08-24 13:45:21 +0530

    Delete module_first.py

--- module_first.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("This is the main() function of First Module and the name of the First Module is {} ".format(__name__))
-    
-if __name__=='__main__':
-    main() 
-else:
-    print("This is outside the main() function of First Module")
